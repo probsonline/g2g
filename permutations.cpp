e9c19bc980eb6633d08decb1c8c6d3bf488c013c
Kaleem Anwar <deepembtech@gmail.com>
2018-02-18 19:47:57 -0800

    cleaned up for g2g submission

--- permutations.cpp
+++ permutations.cpp
@@ -9,29 +9,26 @@ int *starting_permutation;
 
 void get_permutations(int set[], int index, int n){
 
-	if (found) return;
+	if (found) return;	//to break the recursion as soon as the match for the next greater permutation is found
 
-//    cout << "index= " << index << endl;
 	if (index==n) //reached the last character. so print it
 	{
 		found = compare_array(starting_permutation, set, n);
 		if (found)
 		{
-			cout << "found the next permutation ";
+//			cout << "found the next permutation ";
+			print_array(set, n);			
 		}
-		print_array(set, n);
+//		print_array(set, n);
 	}
 	else
 	{
 		//Set the first element to each element in the permutation adn then permutate a ccordingly
 		for (int i = index; i < n; ++i)
 		{
-//			cout << "before swap 1 "; print_array(set, n);
 			swap(set[index], set[i]);
-//			cout << "after swap 1:  " << index << " with " << i; print_array(set, n);
 			get_permutations(set, index+1, n); //Move to the next position (i.e. a 1 less subset now )
 			swap(set[index], set[i]); //restore the order for proper storage
-//			cout << "after swap 2:  " << index << " with " << i; print_array(set, n);
 		}
 	}
 }
@@ -55,7 +52,14 @@ bool check_if_greatest_permutation(int input[], int n)
 
 void print_array(int p[], int n)
 {
+#if 1
+	for (int i = 0; i < n; ++i)
+	{
+		cout << p[i] << " ";
+	}
+	cout << endl;
 
+#else	//nicer output that I like
 	cout << "{ ";
 	for (int i = 0; i < n; ++i)
 	{
@@ -67,9 +71,10 @@ void print_array(int p[], int n)
 			cout << p[i] ;
 		}
 	}
-		cout << " }" << endl ;
-}
+	cout << " }" << endl ;
+#endif
 
+}
 
 bool compare_array(int sample[], int comparison[], int n)
 {
@@ -77,7 +82,6 @@ bool compare_array(int sample[], int comparison[], int n)
 	int counter=0;
 	bool match=false;
 	while(i<n && sample[i]==comparison[i]){
-//		cout << "Checking that " << comparison[i] << " at " << i << " is " << sample[i] << endl;
 		i++;
 	}
 
@@ -112,17 +116,15 @@ void get_factorial_nums(int n, int set[]){
 int main()
 {
 
-    // Note that size of arr[] is considered 200 according to
-    // the constraints mentioned in problem statement.
     int t, n;
     int * p;
 
     // Input the number of test cases you want to run
     cin >> t;
 
-    if (t < 1)
+    if (t < 1 || t > 40)
     {
-    	cout << "Invalid number for test case input. Enter at leat one test case.\n";
+    	cout << "Invalid number for test case input. Enter between 1 to 40 test cases.\n";
     	return 0;
     }
 
@@ -132,7 +134,7 @@ int main()
         // Input the number for which to calculate the pemutations
         cin >> n;
 
-        if (n > 0)
+        if (n > 0 && n <= 100)
         {
         	//Allocate memory for the permutation entry
         	starting_permutation = new int[n];
@@ -141,26 +143,26 @@ int main()
 	        {
 	        	cin >> starting_permutation[i];
 	        }
-	        cout << "Input permutation: ";
-	        print_array(starting_permutation, n);
+//	        cout << "Input permutation: ";
+//	        print_array(starting_permutation, n);
 
 	        p = new int[n];
 	        get_factorial_nums(n,p);
 	        if (check_if_greatest_permutation(starting_permutation, n))
 	        {
-		        cout << "The input is already the greated permutation. The sorted order is ";
+//		        cout << "The input is already the greated permutation. The sorted order is ";
 		        print_array(p, n);
 	        }
 	        else
 	        {
-		        cout << "Findng permutations"  << endl;        //Find until what number the permutation is in order 
+//		        cout << "Findng permutations"  << endl;        //Find until what number the permutation is in order 
 		        get_permutations(p, 0, n);
 	        }
 
         }
         else
         {
-        	cout << "invalid input. the number must be positive" << endl;
+        	cout << "invalid input. the number must be between 1 to 100 " << endl;
         }
     }
     return 0;
