7ca673bebfa428f2029484e7a2ff9d3161bb0746
Kaleem Anwar <deepembtech@gmail.com>
2018-02-18 16:48:15 -0800

    finding the next big permutation based upon the input permutation. Not yet working fully

--- permutations.cpp
+++ permutations.cpp
@@ -35,7 +35,7 @@ int get_factorial_recursive(int n){
 }
 
 
-void print_permutation(int p[], int n){
+void print_array(int p[], int n){
 
 	cout << "{ ";
 	for (int i = 0; i < n; ++i)
@@ -58,40 +58,114 @@ void get_permutations(int set[], int index, int n){
 //    cout << "index= " << index << endl;
 	if (index==n) //reached the last character. so print it
 	{
-		print_permutation(set, n);
+		print_array(set, n);
 	}
 	else
 	{
 		//Set the first element to each element in the permutation adn then permutate a ccordingly
 		for (int i = index; i < n; ++i)
 		{
-//			cout << "before swap 1 "; print_permutation(set, n);
+//			cout << "before swap 1 "; print_array(set, n);
 			swap(set[index], set[i]);
-//			cout << "after swap 1:  " << index << " with " << i; print_permutation(set, n);
+//			cout << "after swap 1:  " << index << " with " << i; print_array(set, n);
 			get_permutations(set, index+1, n); //Move to the next position (i.e. a 1 less subset now )
 			swap(set[index], set[i]); //restore the order for proper storage
-//			cout << "after swap 2:  " << index << " with " << i; print_permutation(set, n);
+//			cout << "after swap 2:  " << index << " with " << i; print_array(set, n);
 		}
 	}
 }
 
-int main(int argc, char const *argv[])
+
+//Get from the input permutation, what subset can be ontained to get the next permutation
+int get_sub_permutation_start(int p[], int n)
 {
-	int n;
-	cout << "Enter the number " << endl;
-	cin >> n;
+	int index=0;
+#if 1
 
-	cout << "factorial of the numebr is " << get_factorial_recursive(n) << endl;
+	while(p[index] != n)
+	{
+		index++;
+	}
 
-	int *set = new int[n];
-	get_factorial_nums(n, set);
-	cout << "numbers in the factorial are ";
-	print_permutation(set, n);
+	return (index !=0)? p[index-1]: p[index]; //this is not necessarily correct.
 
-	cout << "permutations of the number are" << endl;
-	get_permutations(set, 0, n);
+#else
+	while(p[i] != n && (p[i+1] > p[i]))
+	{
+		index = i;
+		i++;
+	}
 
-//	delete [] set;
-	delete set;
-	return 0;
+	return (p[index] == n) ?
+
+	//run to n-1 to avoid array out of bound issue
+	for (int i = 0; i < n-1; ++i)
+	{
+		if (p[i] != n && (p[i+1] > p[i]))
+		{
+			index = i;
+			/* code */
+		}
+	}
+#endif	
+}
+
+
+int main()
+{
+
+    // Note that size of arr[] is considered 200 according to
+    // the constraints mentioned in problem statement.
+    int t, n;
+    int *permutations_list;
+    int *starting_permutation;
+    int * p;
+
+//    int arr[100];
+
+    // Input the number of test cases you want to run
+    cin >> t;
+
+    // One by one run for all input test cases
+    while (t--)
+    {
+        // Input the number for which to calculate the pemutations
+        cin >> n;
+
+        if (n > 0)
+        {
+        	//Allocate memory for the permutation entry
+        	starting_permutation = new int[n];
+	        //Input the starting permutation
+	        for (int i = 0; i < n; ++i)
+	        {
+	        	cin >> starting_permutation[i];
+	        }
+
+        }
+        else
+        {
+        	cout << "invalid input" << endl;
+        }
+
+        //Calculate how many permutations are possible
+//        cout << "This number has " << get_factorial_recursive(n) << " permutations " << endl;
+        p = new int[n];
+        cout << "This number has " << get_factorial_nums(n, p) << " permutations " << endl;        //Find until what number the permutation is in order 
+        get_permutations(p, 0, n);
+
+        cout << "this was the permutation input (as starting permutation";
+        print_array(starting_permutation, n);
+
+        //Find permutations of the the subset from where the next greater one will start
+        int m = get_sub_permutation_start(starting_permutation, n);
+        cout << "the sub permutation number is " << m << endl;
+//        int *sub_p = new int[m];
+//        cout << "This sub has these numbers for permutations " ;
+//        get_factorial_nums(m, sub_p);
+//        get_permutations(sub_p, 0, m); //Can we pass a non zero value for index and get the next permutation?
+        get_permutations(p, m-1, n); //Can we pass a non zero value for index and get the next permutation?
+    }
+
+    return 0;
 }
